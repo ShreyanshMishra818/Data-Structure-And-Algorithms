459d8fec43277514ebecbbe697f2269d00c84196
Shreyansh Mishra <130340454+ShreyanshMishra818@users.noreply.github.com>
2024-03-27 15:32:25 +0530

    Time: 1 ms (73.57%), Space: 45.2 MB (87.27%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,25 +1,41 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int n=matrix.length;
-        int m=matrix[0].length;
-        int row[]=new int[n];
-        int col[]=new int[m];
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==0){
-                    row[i]=-1;
-                    col[j]=-1;
-                }
+    int matrixcopy[][]=new int[matrix.length][matrix[0].length];
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                matrixcopy[i][j]=matrix[i][j];
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(row[i]==-1 || col[j]==-1){
-                    matrix[i][j]=0;
-                }
+      for(int i=0;i<matrix.length;i++){
+          for(int j=0;j<matrix[0].length;j++){
+              if(matrix[i][j]==0){
+                  int k=i,l=j;
+                  while( k<matrix.length){
+                      matrixcopy[k][l]=0;
+                      k++;
+                  }
+                  k=i;l=j;
+                  while(k>=0){
+                      matrixcopy[k][l]=0;
+                      k--;
+                  }
+                  k=i;l=j;
+                  while(l<matrix[0].length){
+                      matrixcopy[k][l]=0;
+                      l++;
+                  }
+                  k=i;l=j;
+                  while(l>=0){
+                      matrixcopy[k][l]=0;
+                      l--;
+                  }
+              }
+          }
+      }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                matrix[i][j]=matrixcopy[i][j];
             }
         }
-        
     }
 }
